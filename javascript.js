929ec804f6e1fb205bf11fafd5237a0b56a76f0c
David Hauck <dhauck@mac.com>
2020-01-26 20:30:15 -0500

    added friend finder and banana knife fight to portfolio

--- javascript.js
+++ javascript.js
@@ -19,6 +19,8 @@ $("#box3").hide();
 $("#box4").hide();
 $("#box5").hide();
 $("#box6").hide();
+$("#box7").hide();
+$("#box8").hide();
 
 // ...then call the functions to animate them after various intervals
 setTimeout(fadeMain, 500);
@@ -29,6 +31,8 @@ setTimeout(fadeBox3, 1500);
 setTimeout(fadeBox4, 1500);
 setTimeout(fadeBox5, 2000);
 setTimeout(fadeBox6, 2500);
+setTimeout(fadeBox7, 3000);
+setTimeout(fadeBox8, 3500);
 
 // functions to show all the page elements
 function fadeMain() {  
@@ -37,31 +41,39 @@ function fadeMain() {
 
 function fadeBox1() {  
     $("#box1").slideDown(1500);
-}
+};
 
 function fadeBox2() {  
     $("#box2").slideDown(1500);
-}
+};
 
 function fadeBox3() {  
     $("#box3").slideDown(1500);
-}
+};
 
 function fadeBox4() {  
     $("#box4").slideDown(1500);
-}
+};
 
 function fadeBox5() {  
     $("#box5").slideDown(1500);
-}
+};
 
 function fadeBox6() {  
     $("#box6").slideDown(1500);
-}
+};
+
+function fadeBox7() {  
+    $("#box7").slideDown(1500);
+};
+
+function fadeBox8() {  
+    $("#box8").slideDown(1500);
+};
 
 function fadeFooter() { 
     $(".footer").fadeIn(1500);
-}
+};
 
 
 });
